0e7104699f267d28023e9c35d7a3645efe6f7f1a
Owen Plett <owen.plett@gmail.com>
2025-07-25 08:38:10 -0600

    fixed lang .json file

--- src/main/java/net/je/JourneysEnd.java
+++ src/main/java/net/je/JourneysEnd.java
@@ -50,7 +50,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(JourneysEnd.MODID)
 public class JourneysEnd {
 
